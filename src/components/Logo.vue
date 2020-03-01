208b010e9bd8d18fe7d19fa5cdbb2620c17329fa
paulloclara <paulloclara@gmail.com>
2020-03-01 11:14:30 -0300

    :fire: Att subtitle

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -1,9 +1,10 @@
 <template>
   <div class="text-center my-6">
-    <h1 class="display-3">NameGat<SetTheme />r</h1>
-    <h6 class="subtitle-2 mt-2">
-      Gerador de nomes utilizando Vue.js, GraphQL e Node
-    </h6>
+    <h1 class="display-3">NameGat<m-set-theme />r</h1>
+
+    <h2 class="subtitle-2 mt-2">
+      Gerador de nomes utilizando Vue.js ❤
+    </h2>
   </div>
 </template>
 
@@ -13,7 +14,7 @@ import SetTheme from "@/components/SetTheme";
 export default {
   name: "Logo",
   components: {
-    SetTheme
+    "m-set-theme": SetTheme
   }
 };
 </script>
