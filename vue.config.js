2544170b8c47412af57d48d10a2d385b4c06f35f
paulloclara <paulloclara@gmail.com>
2020-03-04 11:28:53 -0300

    :rocket: Initial pwa configuration

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,20 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/name-gator/" : "/",
+  publicPath:
+    process.env.NODE_ENV !== "production" ||
+    process.argv.includes("--local-server")
+      ? "/"
+      : "/name-gator/",
   outputDir: "dist",
   lintOnSave: false,
-  transpileDependencies: ["vuetify"]
+  transpileDependencies: ["vuetify"],
+  pwa: {
+    name: "NameGator",
+    display: "standalone",
+    themeColor: "#87c440",
+    msTileColor: "#87c440",
+    manifestOptions: {
+      short_name: "NG",
+      background_color: "#000000"
+    }
+  }
 };
